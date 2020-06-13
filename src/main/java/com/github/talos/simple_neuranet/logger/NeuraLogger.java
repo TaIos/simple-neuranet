69e450a8d8c164663325dd6d8e0b88c8884c634a
Martin Safranek <martin.safranek@itelligence.cz>
2020-06-13 15:55:08 +0200

    upgraded to log4j version 2
    * log4j version 1 has security vulnerabilities, upgrade to version 2 was
    needed
    * changed configuration file to log4j2.json
        * added three JACKSON dependencies for reading this file

--- src/main/java/com/github/talos/simple_neuranet/logger/NeuraLogger.java
+++ src/main/java/com/github/talos/simple_neuranet/logger/NeuraLogger.java
@@ -1,34 +1,47 @@
 package com.github.talos.simple_neuranet.logger;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Log4j wrapper for logging.
  */
 public class NeuraLogger {
-	final static Logger logger = Logger.getLogger(com.github.talos.simple_neuranet.logger.NeuraLogger.class);
+	final static Logger logger = LogManager.getLogger(com.github.talos.simple_neuranet.logger.NeuraLogger.class.getCanonicalName());
 
 	// ========================================================================================================================
 	// LOGGING
 	// ========================================================================================================================
 
 	public static void error(String msg) {
-		logger.error(msg);
+		if (logger.isErrorEnabled()) {
+			logger.error(msg);
+		} else {
+			System.out.println("ERROR LOGGING NOT ENABLED: " + msg);
+		}
 	}
 
 	public static void info(String msg) {
 		if (logger.isInfoEnabled()) {
 			logger.info(msg);
+		} else {
+			System.out.println("INFO LOGGING NOT ENABLED: " + msg);
 		}
 	}
 
 	public static void warning(String msg) {
-		logger.warn(msg);
+		if (logger.isWarnEnabled()) {
+			logger.warn(msg);
+		} else {
+			System.out.println("WARNING LOGGING NOT ENABLED: " + msg);
+		}
 	}
 
 	public static void debug(String msg) {
 		if (logger.isDebugEnabled()) {
 			logger.debug(msg);
+		} else {
+			System.out.println("DEBUG LOGGING NOT ENABLED: " + msg);
 		}
 	}
 
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Class.forName(org.apache.log4j.Logger.class.getCanonicalName());
 			System.out.println("OK");
 		} catch (Exception e) {
 			System.err.println(e.toString());
